692f9fd13bbe570ecc675f78dce20a829292a41d
spi971 <sandypierre@protonmail.com>
2024-02-16 12:53:21 +0100

    Add new execices

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,10 @@
 class Lasagna
 {
-    // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven() => 40;
 
-    // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int timeInOven) => this.ExpectedMinutesInOven() - timeInOven;
 
-    // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int layers) => layers * 2;
 
-    // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int layers, int timeInOven) => this.PreparationTimeInMinutes(layers) + timeInOven;
 }
--- csharp/lucians-luscious-lasagna/obj/Debug/net7.0/LuciansLusciousLasagna.AssemblyInfo.cs
+++ csharp/lucians-luscious-lasagna/obj/Debug/net7.0/LuciansLusciousLasagna.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LuciansLusciousLasagna")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e695dc823a103c32283a843dcb4435ce5a58ed0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e700579e386f1cad67a63d63d413abab41a535a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LuciansLusciousLasagna")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LuciansLusciousLasagna")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
